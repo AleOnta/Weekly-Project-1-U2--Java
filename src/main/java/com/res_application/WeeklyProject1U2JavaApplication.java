60b328ddcbe245d4beca5bd501945576e8971cd9
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-14 21:09:39 +0200

    initialized the project

---
+++ src/main/java/com/res_application/WeeklyProject1U2JavaApplication.java
@@ -0,0 +1,13 @@
+package com.res_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyProject1U2JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyProject1U2JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/res_application/WeeklyProject1U2JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.res_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyProject1U2JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
