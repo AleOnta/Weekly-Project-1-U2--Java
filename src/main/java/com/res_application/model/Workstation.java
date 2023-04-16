14be6a9c88aff495da90843f300799105e207fd7
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-16 23:18:58 +0200

    adds cascade and fetch to Workstation OneToMany and toString

--- src/main/java/com/res_application/model/Workstation.java
+++ src/main/java/com/res_application/model/Workstation.java
@@ -7,9 +7,11 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -24,7 +26,6 @@ import lombok.ToString;
 @Data
 @AllArgsConstructor
 @RequiredArgsConstructor
-@ToString
 @Builder
 public class Workstation {
 	
@@ -42,9 +43,18 @@ public class Workstation {
 	
 	private Integer max_sit;
 	
-	@ManyToOne(cascade = CascadeType.ALL)
+	@ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REFRESH})
+	@JoinColumn(name = "building_id")
 	private Building building;
 	
-	@OneToMany(mappedBy = "location", cascade = CascadeType.REFRESH)
+	@OneToMany(mappedBy = "location", cascade = {CascadeType.MERGE, CascadeType.REFRESH}, orphanRemoval = true)
 	private final List<Reservation> reservations = new ArrayList<Reservation>();
+
+	@Override
+	public String toString() {
+		return "Workstation [id=" + id + ", description=" + description + ", type=" + type + ", status=" + status
+				+ ", max_sit=" + max_sit + ", building=" + building.getId() + "]";
+	}
+	
+	
 }
