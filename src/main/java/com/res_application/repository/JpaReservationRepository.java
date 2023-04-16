3d4877c58f0741dbf4760c085bfb17587e673c56
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-16 23:19:45 +0200

    adds finders and query to ReservationRepo

--- src/main/java/com/res_application/repository/JpaReservationRepository.java
+++ src/main/java/com/res_application/repository/JpaReservationRepository.java
@@ -1,10 +1,23 @@
 package com.res_application.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.res_application.model.Reservation;
+import com.res_application.model.User;
+import com.res_application.model.Workstation;
 
 @Repository
 public interface JpaReservationRepository extends CrudRepository<Reservation, Long> {
 
+	// finders 
+	
+	public List<Reservation> findByLocation(Workstation location);
+	
+	public List<Reservation> findByDate(LocalDate date);
+	
+	public List<Reservation> findByOwner(User owner);
+	
 }
