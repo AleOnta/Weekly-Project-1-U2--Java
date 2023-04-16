3c04c3506b47f0b3d12e5ad71cd40fbed90dcf8b
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-16 23:17:09 +0200

    Update config beans of Workstation class

--- src/main/java/com/res_application/config/WorkstationConfig.java
+++ src/main/java/com/res_application/config/WorkstationConfig.java
@@ -1,8 +1,12 @@
 package com.res_application.config;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+
+import com.github.javafaker.Faker;
 import com.res_application.model.Building;
 import com.res_application.model.E_WorkstationStatus;
 import com.res_application.model.E_WorkstationType;
@@ -15,6 +19,34 @@ public class WorkstationConfig {
 	@Scope("prototype")
 	public Workstation customWorkstation() {
 		return new Workstation();
+	}
+	
+	@Bean("fakeWorkstation") 
+	@Scope("prototype") 
+	public Workstation fakeWorkstation() {
+		Faker fake = Faker.instance(new Locale("it-IT"));
+		Integer random = fake.number().numberBetween(10, 200);
+		E_WorkstationType type = null;
+		
+		 if(random <= 50) {
+			 type = E_WorkstationType.PRIVATE;
+		 } else if (random > 50 && random <= 100) {
+			 type = E_WorkstationType.HALL;
+		 } else if (random > 100 && random <= 150) {
+			 type = E_WorkstationType.MEETINGS;
+		 } else {
+			 type = E_WorkstationType.OPENSPACE;
+		 }
+		
+		Workstation ws = 
+				Workstation.builder()
+				.id(null)
+				.description(fake.lorem().characters(25))
+				.type(type)
+				.status(E_WorkstationStatus.AVAILABLE)
+				.max_sit(random)
+				.build();
+		return ws;
 	}
 	
 	@Bean("paramsWorkstation")
