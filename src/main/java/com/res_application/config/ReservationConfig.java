2e329ce56785609a8261b778efeb5d8d32baf2f3
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-16 23:16:30 +0200

    Update config beans of Reservation class

--- src/main/java/com/res_application/config/ReservationConfig.java
+++ src/main/java/com/res_application/config/ReservationConfig.java
@@ -1,8 +1,16 @@
 package com.res_application.config;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+
+import com.github.javafaker.Faker;
 import com.res_application.model.Reservation;
 import com.res_application.model.User;
 import com.res_application.model.Workstation;
@@ -16,10 +24,23 @@ public class ReservationConfig {
 		return new Reservation();
 	}
 	
+	@Bean("fakeReservation")
+	@Scope("prototype")
+	public Reservation fakeReservation() {
+		Faker fake = Faker.instance(new Locale("it-IT"));
+		Reservation res = Reservation.builder()
+				.id(null)
+				.owner(null)
+				.location(null)
+				.date(LocalDate.of(2023, 04, fake.number().numberBetween(1, 30)))
+				.build();
+		return res;
+	}
+	
 	@Bean("paramsReservation")
 	@Scope("prototype")
-	public Reservation paramsReservation(User u, Workstation w) {
-		return new Reservation(null, u, w);
+	public Reservation paramsReservation(User u, Workstation w, LocalDate res_date) {
+		return new Reservation(null, u, w, res_date);
 	}
 	
 }
