89a5273a7330f1c4a13c6ae439c0f6dacce6c545
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-16 23:19:27 +0200

    adds finders and query to BuildingRepos

--- src/main/java/com/res_application/repository/JpaBuildingRepository.java
+++ src/main/java/com/res_application/repository/JpaBuildingRepository.java
@@ -1,5 +1,8 @@
 package com.res_application.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.res_application.model.Building;
@@ -7,4 +10,21 @@ import com.res_application.model.Building;
 @Repository
 public interface JpaBuildingRepository extends CrudRepository<Building, Long> {
 
+	// finders
+	public Building findByName(String name);
+	
+	public Building findByAddress(String address);
+	
+	public List<Building> findByCity(String city);
+	
+	// complex finders
+	
+	
+	// custom queries
+	
+	@Query(value = "SELECT b FROM Building b ORDER BY random() LIMIT 1")
+	public Building getRandomBuilding();
+	
+	@Query(value = "SELECT COUNT(b) FROM Building b")
+	public Integer countHowManyBuilding();
 }
