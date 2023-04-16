0726f218a0b0483092b903a0985ee65862da23bb
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-16 23:20:05 +0200

    adds finders and query to WorkstationRepo

--- src/main/java/com/res_application/repository/JpaWorkstationRepository.java
+++ src/main/java/com/res_application/repository/JpaWorkstationRepository.java
@@ -1,10 +1,33 @@
 package com.res_application.repository;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import com.res_application.model.E_WorkstationStatus;
+import com.res_application.model.E_WorkstationType;
 import com.res_application.model.Workstation;
 
 @Repository
 public interface JpaWorkstationRepository extends CrudRepository<Workstation, Long> {
-
+	
+	// finders
+	public List<Workstation> findByStatus(E_WorkstationStatus status);
+	
+	public List<Workstation> findByType(E_WorkstationType type);
+	
+	// complex finders
+	
+	// custom queries
+	
+	@Query(value = "SELECT w FROM Workstation w INNER JOIN w.building b WHERE Lower(b.city) LIKE Lower('%' || :city || '%')")
+	public List<Workstation> getByCity(String city);
+	
+	@Query(value = "SELECT w FROM Workstation w ORDER BY random() LIMIT 1")
+	public Workstation getRandomWorkstation();
+		
+	@Query(value = "SELECT COUNT(w) FROM Workstation w")
+	public Integer countHowManyWorkstation();
+	
 }
